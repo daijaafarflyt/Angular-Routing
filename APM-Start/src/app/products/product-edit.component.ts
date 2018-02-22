cb0822f9c945b61b12def1da60db5485c9e306c6
Dai Jaafar <daijaafar@gmail.com>
2018-02-22 17:40:27 +0800

    in progress still

--- APM-Start/src/app/products/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit.component.ts
@@ -15,6 +15,7 @@ export class ProductEditComponent {
     errorMessage: string;
 
     product: IProduct;
+    private dataIsValid:  { [key: string]: boolean } = {};
 
     constructor(private productService: ProductService,
                 private messageService: MessageService,
@@ -22,21 +23,9 @@ export class ProductEditComponent {
                 private router: Router) { }
 
     ngOnInit(): void {
-        
-        this.route.params.subscribe(
-            params => {
-                let id = +params['id'];
-                this.getProduct(id);
-            }
-        )
-    }
-
-    getProduct(id: number): void {
-        this.productService.getProduct(id)
-            .subscribe(
-                (product: IProduct) => this.onProductRetrieved(product),
-                (error: any) => this.errorMessage = <any>error
-            );
+        this.route.data.subscribe(data => {
+            this.onProductRetrieved(data['product']);
+        })
     }
 
     onProductRetrieved(product: IProduct): void {
@@ -64,8 +53,17 @@ export class ProductEditComponent {
         }
     }
 
+    isValid(path: string): boolean {
+        this.validate();
+        if (path) {
+            return this.dataIsValid[path];
+        }
+        return (this.dataIsValid &&
+            Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
+    }
+
     saveProduct(): void {
-        if (true === true) {
+        if (this.isValid(null)) {
             this.productService.saveProduct(this.product)
                 .subscribe(
                     () => this.onSaveComplete(`${this.product.productName} was saved`),
@@ -83,5 +81,27 @@ export class ProductEditComponent {
 
         // Navigate back to the product list
         this.router.navigate(['/products']);
+    }
+
+    validate(): void {
+        //Clear the validation object
+        this.dataIsValid = {};
+
+        // 'info' tab
+        if (this.product.productName &&
+            this.product.productName.length >= 3 &&
+            this.product.productCode) {
+            this.dataIsValid['info'] = true;
+        } else {
+            this.dataIsValid['info'] = false;
+        }
+
+        // 'tags' tab
+        if (this.product.category &&
+            this.product.category.length >= 3) {
+            this.dataIsValid['tags'] = true;
+        } else {
+            this.dataIsValid['tags'] = false;
+        }
     }
 }
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit.component';
+import { ProductEditInfoComponent } from './product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit-tags.component';
 
 import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductService } from './product.service';
+import { ProductResolver } from './product-resolver.service';
 
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
@@ -15,18 +18,44 @@ import { RouterModule } from '@angular/router';
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'products/:id/edit', component: ProductEditComponent }
+      { 
+        path: 'products/:id', 
+        component: ProductDetailComponent, 
+        resolve: { product: ProductResolver }
+      },
+      {
+        path: 'products/:id/edit', 
+        component: ProductEditComponent,
+        resolve: { product: ProductResolver },
+        children: [
+          {
+            path: '',
+            redirectTo: 'info',
+            pathMatch: 'full'
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
+      }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent,
     ProductFilterPipe
   ],
   providers: [
-    ProductService
+    ProductService,
+    ProductResolver
   ]
 })
-export class ProductModule {}
+export class ProductModule { }

5ef9a7a05058c39764641c1228409ab4dcd1891b
Dai Jaafar <daijaafar@gmail.com>
2018-02-22 18:48:06 +0800

    4 modules left

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -17,29 +17,27 @@ import { RouterModule } from '@angular/router';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
       { 
-        path: 'products/:id', 
-        component: ProductDetailComponent, 
-        resolve: { product: ProductResolver }
-      },
-      {
-        path: 'products/:id/edit', 
-        component: ProductEditComponent,
-        resolve: { product: ProductResolver },
-        children: [
+        path: 'products', 
+        children : [
           {
             path: '',
-            redirectTo: 'info',
-            pathMatch: 'full'
+            component: ProductListComponent
           },
           {
-            path: 'info',
-            component: ProductEditInfoComponent
+            path: ':id', 
+            component: ProductDetailComponent, 
+            resolve: { product: ProductResolver }
           },
           {
-            path: 'tags',
-            component: ProductEditTagsComponent
+            path: ':id/edit', 
+            component: ProductEditComponent,
+            resolve: { product: ProductResolver },
+            children: [
+              { path: '', redirectTo: 'info', pathMatch: 'full' },
+              { path: 'info', component: ProductEditInfoComponent },
+              { path: 'tags', component: ProductEditTagsComponent }
+            ]
           }
         ]
       }
